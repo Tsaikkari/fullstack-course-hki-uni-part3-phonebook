cf80f77800295dd57683b468620733c37bb713f0
Tsaikkari <trospe@gmx.de>
2020-08-05 01:43:40 +0200

    Build

---
+++ build/precache-manifest.f997eec0d2607f755bf2b42f3bcf17c5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5778b104ea89d7384792793a1ee75f55",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e56dfe2072799410b940",
+    "url": "/static/css/main.c4e3778b.chunk.css"
+  },
+  {
+    "revision": "c164db0d716c10a5f408",
+    "url": "/static/js/2.f7f5c33f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f7f5c33f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e56dfe2072799410b940",
+    "url": "/static/js/main.e98626a1.chunk.js"
+  },
+  {
+    "revision": "f4d2ac87a76b6331fd91",
+    "url": "/static/js/runtime-main.4234e0d5.js"
+  }
+]);
